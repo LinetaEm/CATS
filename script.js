b8daf28c74399321250bac84f15357be8c6cc6f4
LinetaEm <143501281+LinetaEm@users.noreply.github.com>
2023-10-02 11:55:44 +0300

    facts about cats

---
+++ script.js
@@ -0,0 +1,23 @@
+const catFactForm = document.getElementById("catFact");
+const factContainer = document.getElementById("factContainer");
+const getFactButton = document.getElementById("getRandom");
+
+function getCatFact() {
+    fetch("https://catfact.ninja/fact")
+        .then(response => response.json())
+        .then(data => {
+            const paragraph = document.createElement("p");
+            paragraph.textContent = data.fact;
+            factContainer.appendChild(paragraph);
+        });
+}
+
+catFactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    getCatFact();
+});
+
+getFactButton.addEventListener("click", function () {
+    factContainer.innerHTML = "";
+    getCatFact();
+});
